7439dfe7c2468a723421522cda71625047c7e986
Farzad Sanjarani <farzadsanjarani3@gmail.com>
2022-12-29 10:29:01 +0330

    Add files via upload

---
+++ src/redux/user/UserActions.js
@@ -0,0 +1,37 @@
+import { FETCH_USER_FAILURE, FETCH_USER_SUCCESS, FETCH_USER_REQUEST } from "./UserTypes"
+import axios from "axios"
+
+export const fetchUserRequest = () =>{
+    return{
+        type: FETCH_USER_REQUEST
+    }
+}
+
+const fetchUserSuccess = user =>{
+    return{
+        type: FETCH_USER_SUCCESS,
+        payload: user
+    }
+}
+
+const fetchUserFailure = err =>{
+    return{
+        type: FETCH_USER_FAILURE,
+        payload: err
+    }
+}
+
+export const fetchUsers = () =>{
+    return (dispatch) =>{
+        dispatch(fetchUserRequest)
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(res => {
+                const users = res.data
+                dispatch(fetchUserSuccess(users))
+            })
+            .catch(err =>{
+                const errMessage = err.message
+                dispatch(fetchUserFailure(errMessage))
+            })
+    }
+}
---
+++ src/redux/user/UserReducer.js
@@ -0,0 +1,32 @@
+import { FETCH_USER_FAILURE, FETCH_USER_REQUEST, FETCH_USER_SUCCESS } from "./UserTypes"
+
+const initialState = {
+    loading: false,
+    user: [],
+    err: ''
+}
+
+const reducer = (state = initialState, action) =>{
+    switch(action.type) {
+        case FETCH_USER_REQUEST:
+            return{
+                ...state,
+                loading:true
+            }
+        case FETCH_USER_SUCCESS:
+            return{
+                loading:false,
+                user: action.payload,
+                err:''
+            }
+        case FETCH_USER_FAILURE:
+            return{
+                loading: false,
+                user: [],
+                err: action.payload
+            }
+        default: return state
+    }
+}
+
+export default reducer
